6b04fc7f14e9a7f24d16a983255a2a5e05d3f8d5
ABIF-McGill <64212264+ABIF-McGill@users.noreply.github.com>
2024-03-22 17:19:48 -0400

    updated script
    just wells - no antibody names

--- calculate_ratios_and_plotting_template.R
+++ calculate_ratios_and_plotting_template.R
@@ -9,7 +9,7 @@ library(reshape2)
 
 #setwd("")
 
-data <- read.csv('')
+data <- read.csv('Results_w3_intensities_2024222_0_abNames_cp2_test_plt46.csv')
 
 
 data$well_scene <- paste(data$well, data$scene, sep="_")
@@ -20,25 +20,25 @@ data$well_scene <- paste(data$well, data$scene, sep="_")
 # Preprocess data - remove unwanted rows
 data <- data %>%
   filter(Min > 0,
-         antibody != 0,
+         #antibody != 0,
          Circ. < 0.9,
          Max < 40000,
          w1_max < 40000,
          w2_max < 40000,
-         w4_max < 40000,
-         !grepl("__", antibody))
+         w4_max < 40000)
+         #!grepl("__", antibody))
 
 
 #Get rid of unusable wells
 #data<-subset(data, data$well != "D02")
 
 
-data <- data[!grepl("__", data$antibody),]
+#data <- data[!grepl("__", data$antibody),]
 
 # summarise data
 
 summary_ab_scene_data <- data %>%
-  group_by(antibody, scene, celltype) %>%
+  group_by(well, scene, celltype) %>%
   #group_by(antibody, celltype) %>%
   summarise(avg = mean(Mean), med = median(Mean), medArea = median(Area), count = length(Area))
 
@@ -46,14 +46,14 @@ summary_ab_scene_data <- subset(summary_ab_scene_data, summary_ab_scene_data$cou
 
 
 summary_ab_data <- data %>%
-  group_by(antibody, celltype) %>%
+  group_by(well, celltype) %>%
   summarise(avg = mean(Mean), med = median(Mean), medArea = median(Area),count = length(Area))
 
 
 
 # for ratios: reshape dataframe
 
-summary_ab_data.wide <- dcast(summary_ab_data, antibody ~ celltype, value.var="med")
+summary_ab_data.wide <- dcast(summary_ab_data, well ~ celltype, value.var="med")
 summary_ab_data.wide$ratio <- summary_ab_data.wide$green / summary_ab_data.wide$red
 summary_ab_scene_data$ab_scene <-  paste(summary_ab_scene_data$antibody, summary_ab_scene_data$scene)
 summary_ab_scene_data.wide <- NULL
@@ -75,22 +75,16 @@ summary_ab_scene_data.wide_2$ratio <- summary_ab_scene_data.wide_2$MedGreen / su
 
 
 summary_ab_data_2 <- summary_ab_scene_data.wide_2 %>%
-  group_by(antibody) %>%
+  group_by(well) %>%
   summarise(avgGreen = mean(MedGreen), avgRed = mean(MedRed), ratio = median(ratio),  numCells = sum(count))
 
 
 
 
-### remove ctrl and 0 rows
-summary_ab_scene_data.wide_2 = subset(summary_ab_scene_data.wide_2, summary_ab_scene_data.wide_2$antibody != "0")
-summary_ab_data_2 = subset(summary_ab_data_2, summary_ab_data_2$antibody != "0")
-
-
-
-ggplot(summary_ab_data_2, aes(antibody, ratio)) +
+ggplot(summary_ab_data_2, aes(well, ratio)) +
   geom_col(color="dark grey",fill="white")+
   geom_jitter(data = summary_ab_scene_data.wide_2, alpha=0.4, width=0.1) +
-  geom_text(aes(x = antibody, y = -0.1, label = round(ratio, digits = 1)), size=3)+
+  geom_text(aes(x = well, y = -0.1, label = round(ratio, digits = 1)), size=3)+
   theme_classic() +
   #ylim(-0.2,4) +
   xlab("")+
